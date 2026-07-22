54177da60803916da116b94558105302df81ee71
David Nathanson <107134818+DaveMatNat@users.noreply.github.com>
2026-07-22 23:54:47 +0700

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def solve(node, depth):
+            # base case
+            if not node:
+                return depth
+            # recursive case
+            return max(solve(node.left, depth+1), solve(node.right, depth+1))
+        return solve(root,0)
+
