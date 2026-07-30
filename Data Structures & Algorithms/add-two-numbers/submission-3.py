e180b80be2e259e05b2ec520d3a0e9df00efc3aa
David Nathanson <107134818+DaveMatNat@users.noreply.github.com>
2026-07-31 01:04:32 +0700

    Add: add-two-numbers - submission-3

---
+++ Data Structures & Algorithms/add-two-numbers/submission-3.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        d1 = l1
+        d2 = l2
+        curr = dummy = ListNode()
+        carry = 0
+        while d1 or d2:
+            num1 = d1.val if d1 else 0
+            num2 = d2.val if d2 else 0
+            s = num1+num2+carry
+            carry = s // 10
+            digit = s % 10
+            curr.next = ListNode(digit)
+            curr = curr.next
+            d1 = d1.next if d1 else None
+            d2 = d2.next if d2 else None
+        if carry != 0:
+            curr.next = ListNode(carry)
+
+        # while d1:
+        #     while carry != 0:
+        #         num1 = d1.val
+        #         s = num1+carry
+        #         carry = s // 10
+        #         digit = s % 10
+        #         curr.next = ListNode(digit)
+        #         curr = curr.next
+        #         d1 = d1.next
+        # while d2:
+        #     while carry != 0:
+        #         num2 = d2.val
+        #         s = num2+carry
+        #         carry = s // 10
+        #         digit = s % 10
+        #         curr.next = ListNode(digit)
+        #         curr = curr.next
+        #         d2 = d2.next
+        return dummy.next
